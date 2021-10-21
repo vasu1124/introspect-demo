7d5c39abc5e8a3454e554267a37e41a5dd4981fb
vasu1124 <vasu1124@actvirtual.com>
2021-10-21 21:53:23 +0200

    rehaul
    Signed-off-by: vasu1124 <vasu1124@actvirtual.com>

---
+++ introspect/istio/istio-install.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+export domainname="*.bnerd4.actvirtual.com"
+export issuer="actvirtual"
+
+cat <<EOF | istioctl manifest generate -o . -f -
+apiVersion: install.istio.io/v1alpha1
+kind: IstioOperator
+metadata:
+  namespace: istio-system
+  name: demo
+spec:
+  profile: demo
+  components:
+    egressGateways:
+    - name: istio-egressgateway
+      enabled: false
+    ingressGateways:
+    - name: istio-ingressgateway
+      enabled: true
+      k8s:
+        service:
+          ports:
+          - name: status-port
+            port: 15021
+            targetPort: 15021
+          - name: http2
+            port: 80
+            targetPort: 8080
+          - name: https
+            port: 443
+            targetPort: 8443
+        serviceAnnotations:
+          dns.gardener.cloud/dnsnames: "${domainname}"
+          dns.gardener.cloud/ttl: "300" 
+          dns.gardener.cloud/class: garden
+          cert.gardener.cloud/issuer: "${issuer}"
+          cert.gardener.cloud/secretname: wildcard-tls
+  meshConfig:
+    accessLogFile: /dev/stdout
+EOF
+
+cat <<EOF | kubectl apply -f -
+apiVersion: networking.istio.io/v1beta1
+kind: Gateway
+metadata:
+  name: ${issuer}-gw
+  namespace: istio-system
+spec:
+  selector:
+    istio: ingressgateway #! use istio default ingress gateway
+  servers:
+  - port:
+      number: 80
+      name: http
+      protocol: HTTP
+    tls:
+      httpsRedirect: true
+    hosts:
+    - "${domainname}"
+  - port:
+      number: 443
+      name: https
+      protocol: HTTPS
+    tls:
+      mode: SIMPLE
+      credentialName: wildcard-tls
+    hosts:
+    - "${domainname}"
+EOF
